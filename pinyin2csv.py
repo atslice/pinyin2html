e1ddfa92aeb88d982217b45ee893b5bf2658f531
atslice <alice.patel2008@gmail.com>
2023-08-07 22:06:09 +0800

    statics

--- pinyin2csv.py
+++ pinyin2csv.py
@@ -6,6 +6,17 @@ class Pinyin2csv():
     def __init__(self) -> None:
         pass
 
+    def write_file(self, _file, _str, encoding = None):
+        """
+            write str to file
+        """
+        if encoding is None:
+            with open(_file, 'w') as writer:
+                writer.write(_str)
+        else:
+            with open(_file, 'w', encoding=encoding) as writer:
+                writer.write(_str)                     
+
     def dump_html(self, chars: str, number: int, out_dir = None, out_name = None):
         """
             Args:
@@ -17,31 +28,40 @@ class Pinyin2csv():
 
         # default mode
         csv_chars, csv_marks, csv_marks_r = self.gen_csv(chars=chars, number=number)
+
         out_file_chars = os.path.join(out_dir, f'{name}_chars.csv')
-        with open(out_file_chars, 'w') as writer:
-            writer.write(csv_chars)
+        self.write_file(_file = out_file_chars, _str = csv_chars)
+
         out_file_chars_gb2312 = os.path.join(out_dir, f'{name}_chars_gb2312.csv')
-        with open(out_file_chars_gb2312, 'w', encoding='gb2312') as writer:
-            writer.write(csv_chars)            
+        self.write_file(_file = out_file_chars_gb2312, _str = csv_chars, encoding='gb2312') 
+
         out_file_pinyin = os.path.join(out_dir, f'{name}_pinyin.csv')            
-        with open(out_file_pinyin, 'w') as writer:
-            writer.write(csv_marks)
+        self.write_file(_file = out_file_pinyin, _str = csv_marks)
+
+        out_file_pinyin_gb2312 = os.path.join(out_dir, f'{name}_pinyin_gb2312.csv')            
+        self.write_file(_file = out_file_pinyin_gb2312, _str = csv_marks, encoding='gb2312')        
+
         out_file_pinyin_r = os.path.join(out_dir, f'{name}_pinyin_r.csv')            
-        with open(out_file_pinyin_r, 'w') as writer:
-            writer.write(csv_marks_r)
+        self.write_file(_file = out_file_pinyin_r, _str = csv_marks_r)
+
         out_file_pinyin_r_gb2312 = os.path.join(out_dir, f'{name}_pinyin_r_gb2312.csv')            
-        with open(out_file_pinyin_r_gb2312, 'w', encoding='gb2312') as writer:
-            writer.write(csv_marks_r)            
+        self.write_file(_file = out_file_pinyin_r_gb2312, _str = csv_marks_r, encoding='gb2312')            
         print(out_file_pinyin_r)
 
         # heteronym as reference
         csv_marks, csv_marks_r = self.gen_heteronym_csv(chars=chars, number=number)
+
         out_file_pinyin = os.path.join(out_dir, f'{name}_pinyin_heteronym.csv')            
-        with open(out_file_pinyin, 'w') as writer:
-            writer.write(csv_marks)
+        self.write_file(_file = out_file_pinyin, _str = csv_marks)
+
+        out_file_pinyin_gb2312 = os.path.join(out_dir, f'{name}_pinyin_heteronym_gb2312.csv')            
+        self.write_file(_file = out_file_pinyin_gb2312, _str = csv_marks, encoding='gb2312')        
+
         out_file_pinyin_r = os.path.join(out_dir, f'{name}_pinyin_heteronym_r.csv')            
-        with open(out_file_pinyin_r, 'w') as writer:
-            writer.write(csv_marks_r)
+        self.write_file(_file = out_file_pinyin_r, _str = csv_marks_r)
+
+        out_file_pinyin_r_gb2312 = os.path.join(out_dir, f'{name}_pinyin_heteronym_r_gb2312.csv')
+        self.write_file(_file = out_file_pinyin_r_gb2312, _str = csv_marks_r, encoding='gb2312')           
         print(out_file_pinyin_r)        
 
 
@@ -175,9 +195,29 @@ def str2csv(chars: str, number: int, name: str):
     print(f'There are {len(chars)} chars to be parsed.') 
     p2h.dump_html(chars=chars, number = number, out_dir=out_dir, out_name = name)
 
+def statics(strings):
+    """
+        Args:
+            strings: list of str
+    """
+    all_string = ''
+    for string in strings:
+        all_string += string
+    print(f'There are {len(all_string)} chars in total')
+    unique_chars = set()
+    for char in all_string:
+        unique_chars.add(char)
+    print(f'There are {len(unique_chars)} unique chars in total')        
+
 def main():
-    chars = '隔江犹唱后庭花回乡偶书其一贺知章离别家乡岁月多近来人事半消磨惟有门前镜湖水春风不改旧时波回乡偶书其二贺知章少小离家老大回乡音无改鬓毛衰儿童相'
-    chars = '见不相识笑问客从何处来黄鹤楼送孟浩然之广陵故人西辞黄鹤楼烟花三月下扬州孤帆远影碧空尽唯见长江天际流李白暮江吟白居易一道残阳铺水中半江瑟瑟半江'
+    strings = [
+        '隔江犹唱后庭花回乡偶书其一贺知章离别家乡岁月多近来人事半消磨惟有门前镜湖水春风不改旧时波回乡偶书其二贺知章少小离家老大回乡音无改鬓毛衰儿童相',
+        '见不相识笑问客从何处来黄鹤楼送孟浩然之广陵故人西辞黄鹤楼烟花三月下扬州孤帆远影碧空尽唯见长江天际流李白暮江吟白居易一道残阳铺水中半江瑟瑟半江',
+        '一日还两岸猿声啼不住轻舟已过万重山寻隐者不遇贾岛松下问童子言师采药去只在此山中云深不知处泊秦淮杜牧烟笼寒水月笼沙夜泊秦淮近酒家商女不知亡国恨'
+    ]
+    statics(strings)
+    return
+    chars = strings[-1]
     name = chars[-7:]
     str2csv(chars=chars, number=10, name = name)
 
