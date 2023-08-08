b2a1a626713b1e380813f502b59d453f50e895eb
atslice <alice.patel2008@gmail.com>
2023-08-08 08:37:54 +0800

    source

--- database_gen.py
+++ database_gen.py
@@ -18,82 +18,23 @@ def strip_string(_str):
             result = f'{result}{char}'
     return result
 
-def str_k1a():
-    string = """一二三
-十木禾
-上下土个
-八入大天
-人火文六
-七儿九无
-口日中
-了子门月
-不开四五
-目耳头米
-见白田电
-也长山出
-飞马鸟
-云公车
-牛羊小少
-巾牙尺毛
-卜又心风
-力手水
-广升足走
-方半巴
-业本平书
-自已东西
-回片皮
-生里果
-几用鱼
-今正雨两
-瓜衣来
-年左右
-"""
+def load_string(_file):
+    """
+        load string from file
+    """
+    with open(_file, 'r') as reader:
+        return reader.read()
+
+def str_k12(name):
+    """
+        Args:
+            name: str, the available values list ['k1a', 'k1s', 'k2a', 'k2s',...,'k12s']
+        read string from k2a.txt and strip white chars, return the only Chinese chars
+    """
+    txt_file = f'source/{name}.txt'
+    string = load_string(txt_file)
     return strip_string(string)
 
-def str_k1s():
-    string = """万丁冬百齐
-说话朋友春高
-你们红绿花草
-爷节岁亲的行
-古声多处知忙
-洗认扫真父母
-爸全关写完家
-看着画笑兴会
-妈奶午合放收
-女太气早去亮
-和语千李秀香
-听唱连远定向
-以后更主意总
-先干赶起明净
-同工专才级队
-蚂蚁前空房网
-诗林童黄闭立
-是朵美我叶机
-她他送过时让
-吗吧虫往得很
-河姐借呢呀哪
-谁怕跟凉量最
-园因为脸阳光
-可石办法找许
-别到那都吓叫
-再象像做点照
-沙海桥竹军苗
-井乡面忘想念
-王从边这进道
-贝原男爱虾跑
-吹地快乐老师
-短对冷淡热情
-拉把给活种吃
-练习苦学非常
-问间伙伴共汽
-分要没位孩选
-北南江湖秋
-只星雪帮请就
-球玩跳桃树刚
-兰各坐座带急
-名发成晚动新
-有在什么变条"""
-    return strip_string(string)
 
 
 def write_database(chars, name):
@@ -136,6 +77,50 @@ def write_database(chars, name):
     with open(out_file, 'w', encoding='utf-8') as writer:
         json.dump(chars_statics, writer, indent=4, ensure_ascii=False)    
 
+def write_database_order(chars, name):
+    """
+        write to json file, keep original order
+        Args:
+            chars: str
+            name: str, the description, and will be used as key name and file basename without extension. 
+        The result json file basename will be '{name}.json'.
+        The result json will be in the format as below:
+            {
+                name:
+                    {
+                        'chars_num': int,
+                        'chars': str
+                    }
+            }
+    """
+
+    # print(chars)
+    print(f'There are {len(chars)} chars in total')
+
+    unique_chars = ''
+    for char in chars:
+        if not char in unique_chars:
+            unique_chars += char
+    print(f'There are {len(unique_chars)} unique chars in total')
+    print(unique_chars)      
+    out_dir = '../p2h_data'
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+
+    out_file = os.path.join(out_dir, f'{name}.json')
+    chars_statics = {
+        name: {
+            'quantity': len(unique_chars),
+            'chars': unique_chars
+        }
+    }
+    with open(out_file, 'w', encoding='utf-8') as writer:
+        json.dump(chars_statics, writer, indent=4, ensure_ascii=False)    
+
+def dump_json(_dict, out_file):
+    with open(out_file, 'w', encoding='utf-8') as writer:
+        json.dump(_dict, writer, indent=4, ensure_ascii=False)     
+
 def relation(str1, str2):
     """
         Args:
@@ -161,14 +146,30 @@ def relation(str1, str2):
     return covered, uncovered, chars_over
 
 def test1():
-    chars_k1a = str_k1a()
-    write_database(chars=chars_k1a, name='k1a')
-    chars_k1s = str_k1s()
-    write_database(chars=chars_k1s, name='k1s')
-    chars_k1 = chars_k1a + chars_k1s
-    write_database(chars=chars_k1, name='k1')
-    covered, uncovered, chars_over = relation(chars_k1a, chars_k1s)
+    k12 = ['k1a', 'k1s', 'k2a']
+    chars_k12 = {}
+    for name in k12:
+        chars = str_k12(name=name)
+        write_database_order(chars=chars, name=name)
+        chars_k12[name] = {
+            'quantity': len(chars),
+            'chars': chars
+        }
+
+    chars_k1 = chars_k12['k1a']['chars'] + chars_k12['k1s']['chars']
+    write_database_order(chars=chars_k1, name='k1')
+    covered, uncovered, chars_over = relation(chars_k12['k1a']['chars'], chars_k12['k1s']['chars'])
     print(f'covered: {covered}')
+
+    chars_k12['k1'] = {
+            'quantity': len(chars_k1),
+            'chars': chars_k1
+        }
+    out_dir = '../p2h_data'
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+    out_file = os.path.join(out_dir, f'k12.json')    
+    dump_json(_dict = chars_k12, out_file=out_file)
 
 def main():
     # strip_str()
---
+++ source.md
@@ -0,0 +1,26 @@
+https://www.an2.net/zi/
+
+/home/mint/Downloads/zi_pack.rar
+
+
+
+# https://github.com/CyangHH/zt
+田字格笔顺生成 田字格笔顺 字帖生成器
+
+基于开源项目: https://github.com/bunian/tianzigebishun
+
+添加了拼音功能 thanks for https://github.com/jifei/Pinyin
+
+演示： https://zt.cyang.net
+
+# https://github.com/jackniu81/PrimarySchoolResource
+
+小学、幼儿园学习资源
+
+A4 可打印资料
+四线三格拼音本, 生字本(拼音本)空田格A4模板, 田格本,汉语拼音本,A4模板 https://github.com/jackniu81/PrimarySchoolResource/tree/master/A4%E7%BA%B8%E6%89%93%E5%8D%B0
+
+Links
+田字格字帖生成器: http://an2.net/zi/
+
+
