68cd2278d9f7fe99e72e8992da23443a85c3fe96
atslice <alice.patel2008@gmail.com>
2023-10-10 17:48:53 +0800

    poet2html

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pinyin2html
 transform Chinese pinyin to html
+
+# break.py
+把诗词json以标点断行
+
+# t2s.py
+把诗词json从繁体转换为简体
---
+++ break.py
@@ -0,0 +1,45 @@
+import os
+import copy
+from opencc import OpenCC
+from pyjsonlib import  load_json, dump_json
+from pyiolib import makedirs
+
+
+def t2s(chars):
+    cc = OpenCC('t2s')  # 使用OpenCC转换繁体为简体
+    return cc.convert(chars)
+
+def paragraphs_break(_list: list):
+    new_p = []
+    for member in _list:
+        new_p.append(member)
+    return new_p
+
+def break_poet(poet: dict):
+    new_poet = copy.deepcopy(poet)
+    for key, value in poet.items():
+        if key == 'paragraphs':  # value is list for such case
+            if type(value) is list:
+                new_poet['paragraphs_break'] = paragraphs_break(value)
+    return new_poet
+
+def break_poets(poets: list):
+    return [break_poet(poet) for poet in poets]
+
+def main():
+    # load input file
+    # input_json = 'input/孟浩然_春.json'
+    name = '古诗接龙'
+    input_json = f'input/{name}.json'
+    poets = load_json(input_json) # list
+
+    poets_break = break_poets(poets)
+
+    out_dir = '../p2h_data'
+    makedirs(out_dir)
+    out_file = os.path.join(out_dir, f'{name}_simple.json')
+    dump_json(_file=out_file, _dict=poets_simple)
+
+
+if __name__ =="__main__":
+    main()
